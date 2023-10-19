e2965ed23eac5c96a7fe4b840b3eb1d10a7229a0
Tabare Sanchez <tabaregabriel@gmail.com>
2023-10-19 11:08:20 -0300

    Creo Enemigo y Waypoints

---
+++ DV_Programacion1_Juego/Assets/Scripts/Enemigo/Enemigo.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Transform[] waypoints;
+    
+    [SerializeField]
+    private Player player;
+
+    private int index = 0;
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 2f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+    private void Patrol()
+    {
+        if (waypoints.Length == 0)
+        {
+            return;
+        }
+
+        Transform target = waypoints[index];
+        transform.position = Vector3.MoveTowards(transform.position, target.position, movementSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            index++;
+            if (index >= waypoints.Length)
+            {
+                index = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponentInParent<Player>();
+        if (player != null)
+        {
+            Debug.Log("Veo al jugador");
+        }
+    }
+
+    public void TakeDamage(int damage) { }
+}
